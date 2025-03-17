22b9a1abaf16590d0fde039f9bb547b2a70cca0e
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-03-17 23:51:01 +0100

    Initial Commit

---
+++ CD_ToDoList/CD_ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CD_ToDoListApp.swift
+//  CD_ToDoList
+//
+//  Created by Miguel Gallego on 17/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CD_ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CD_ToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CD_ToDoList
+//
+//  Created by Miguel Gallego on 17/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
